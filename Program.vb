64e0355445750450b87bf476ebf2e0d4ac2cbcae
Dzah Yaw <82473101+Let-Love-Lead@users.noreply.github.com>
2021-10-20 18:51:55 +0000

    Add files via upload

--- Program.vb
+++ Program.vb
@@ -1,12 +1,37 @@
 Imports System
 
 Module Program
-    Sub Main(args As String())
-        For num As Integer = 1 To 25
-            Console.Write("{0,-4}", num)
-            If (num Mod 5) = 0 Then
-                Console.WriteLine()
+    Sub Main()
+
+        Dim N, i As Integer
+        'N is being used to store the prime numbers & i for our iteration.
+        Dim b As Boolean
+
+        Console.WriteLine(" Prime Number From 1 to 100 = ")
+
+        b = True
+
+        While N <= 100
+            b = True  'we declaring that our N must be less than 100, by seting the boolean b to True.
+            i = 2
+
+            While i < N
+                If N Mod i = 0 Then
+                    b = False
+                    GoTo break
+
+                End If
+                i += 1
+            End While
+break:
+            If b Then
+                Console.WriteLine(N)
             End If
-        Next
+            N += 1
+        End While
+
+
+        Console.ReadKey()
+
     End Sub
 End Module
